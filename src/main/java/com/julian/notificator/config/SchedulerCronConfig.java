01ad09238be747a216ab19eb96ca6753c04afbed
INDRA\jgomezfe <jgomezfe@minsait.com>
2025-11-20 13:19:08 +0100

    first version

---
+++ src/main/java/com/julian/notificator/config/SchedulerCronConfig.java
@@ -0,0 +1,24 @@
+package com.julian.notificator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SchedulerCronConfig {
+
+    private final DailySchedulerProperties props;
+
+    public SchedulerCronConfig(DailySchedulerProperties props) {
+        this.props = props;
+    }
+
+    @Bean
+    String dailyCron() {
+        return String.format("0 %d %d * * *", props.getMinute(), props.getHour());
+    }
+
+    @Bean
+    String dailyZone() {
+        return props.getZone();
+    }
+}
