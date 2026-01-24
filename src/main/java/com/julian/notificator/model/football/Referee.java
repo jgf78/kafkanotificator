cdfb3a5e475a414d64b34846917983af53e79e96
INDRA\jgomezfe <jgomezfe@minsait.com>
2026-01-24 21:57:32 +0100

    Add referee

---
+++ src/main/java/com/julian/notificator/model/football/Referee.java
@@ -0,0 +1,12 @@
+package com.julian.notificator.model.football;
+
+import lombok.Data;
+
+@Data
+public class Referee {
+    private Long id;
+    private String name;
+    private String type;
+    private String nationality;
+}
+
