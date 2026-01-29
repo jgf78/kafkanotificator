e1916ef17f09f8b0714686480c393fad57bdbf6d
INDRA\jgomezfe <jgomezfe@minsait.com>
2026-01-29 07:32:11 +0100

    cosmetic

--- src/main/java/com/julian/notificator/scheduler/LiveMatchNotifier.java
+++ src/main/java/com/julian/notificator/scheduler/LiveMatchNotifier.java
@@ -1,5 +1,7 @@
 package com.julian.notificator.scheduler;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
