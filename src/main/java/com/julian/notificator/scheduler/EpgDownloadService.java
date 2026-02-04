28756954d9acd5debd4079cf3f4227f8dc7915c1
INDRA\jgomezfe <jgomezfe@minsait.com>
2026-02-04 10:22:46 +0100

    Add  @PostConstruct

--- src/main/java/com/julian/notificator/scheduler/EpgDownloadService.java
+++ src/main/java/com/julian/notificator/scheduler/EpgDownloadService.java
@@ -1,13 +1,15 @@
 package com.julian.notificator.scheduler;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.zip.GZIPInputStream;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
@@ -16,6 +18,11 @@ public class EpgDownloadService {
     private static final String EPG_URL = "https://www.tdtchannels.com/epg/TV.xml.gz";
 
     private InputStream lastEpgStream; 
+    
+    @PostConstruct
+    public void init() {
+        downloadEpg(); // descarga al arrancar la app
+    }
 
     @Scheduled(cron = "0 0 */2 * * *") // cada 2 horas
     //@Scheduled(fixedRate = 10000) // cada 10 segundos
