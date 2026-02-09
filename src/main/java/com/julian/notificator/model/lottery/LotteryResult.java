3a2786de6e5445bc1aad473ab49ef39ca8ce716d
INDRA\jgomezfe <jgomezfe@minsait.com>
2026-02-09 08:17:15 +0100

    cosmetic

--- src/main/java/com/julian/notificator/model/lottery/LotteryResult.java
+++ src/main/java/com/julian/notificator/model/lottery/LotteryResult.java
@@ -2,7 +2,6 @@ package com.julian.notificator.model.lottery;
 
 import java.util.List;
 
-import org.hibernate.stat.Statistics;
 import org.telegram.telegrambots.meta.api.objects.games.Game;
 
 public record LotteryResult(
