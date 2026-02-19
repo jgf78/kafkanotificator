482c1f611094d38156f5bef8484f3e22d1b128e4
INDRA\jgomezfe <jgomezfe@minsait.com>
2026-02-19 16:51:51 +0100

    Cosmetic

--- src/main/java/com/julian/notificator/model/subscriber/WebhookEventType.java
+++ src/main/java/com/julian/notificator/model/subscriber/WebhookEventType.java
@@ -1,6 +1,17 @@
 package com.julian.notificator.model.subscriber;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Available webhook event types")
 public enum WebhookEventType {
+
+    @Schema(description = "Triggered when a Telegram text is received")
     TELEGRAM_TEXT_EVENT,
-    LIVE_MATCH_EVENT
+
+    @Schema(description = "Triggered when a live match event occurs")
+    LIVE_MATCH_EVENT,
+
+    @Schema(description = "Triggered when a user registers")
+    USER_REGISTERED_EVENT
 }
+
